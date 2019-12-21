13a0d589fadefa097177090b7251db9fbdc1a372
Bessarab123 <bessarab.kostik@mail.ru>
2019-12-21 18:25:28 +0300

    ДОбавлен Спрайт стены с тестом

--- Board.py
+++ Board.py
@@ -1,11 +1,25 @@
 import pygame
 import os
+import random
 
 WALL = 0
 EMPTY = 1
 EXIT = 3
 ENTER = 2
 COIN = 4
+BLACK = pygame.Color('black')
+
+pygame.init()
+w = 800
+h = 600
+screen = pygame.display.set_mode((w, h))
+
+
+def load_image(name):
+    fullname = os.path.join('data', name)
+    image = pygame.image.load(fullname).convert()
+    image.set_colorkey(BLACK)
+    return image
 
 
 class BoardEditor():
@@ -37,9 +51,24 @@ class BoardEditor():
 
     def on_click(self, cell_coords):
         j, i = cell_coords
-        self.board[i][j] += 1
-        if self.board[i][j] == COIN + 1:
-            self.board[i][j] = WALL
+        press = pygame.key.get_pressed()
+        if any(press):
+            # Если нажата хоть одна клавиша
+            if press[pygame.K_0]:
+                self.board[i][j] = 0
+            elif press[pygame.K_1]:
+                self.board[i][j] = 1
+            elif press[pygame.K_2]:
+                self.board[i][j] = 2
+            elif press[pygame.K_3]:
+                self.board[i][j] = 3
+            elif press[pygame.K_4]:
+                self.board[i][j] = 4
+        else:
+            # Если ни одна клавиша не нажата
+            self.board[i][j] += 1
+            if self.board[i][j] == COIN + 1:
+                self.board[i][j] = WALL
 
     def get_click(self, mouse_pos):
         cell_coords = self.get_cell(mouse_pos)
@@ -57,21 +86,22 @@ class BoardEditor():
         return x1, y1
 
 
-class Object(pygame.sprite.Sprite):
+class Wall(pygame.sprite.Sprite):
+    wall_1 = load_image('WALL_1.png')
+    wall_2 = load_image('WALL_2.png')
+
+    def __init__(self, x, y, alpha, *group):
+        super().__init__(group)
+        self.image = pygame.transform.rotate(random.choice((Wall.wall_1, Wall.wall_2)), alpha)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 
 
-
-def load_image(name):
-    fullname = os.path.join('data', name)
-    image = pygame.image.load(fullname).convert_alpha()
-    return image
-
-
-pygame.init()
-w = 800
-h = 600
-screen = pygame.display.set_mode((w, h))
+all_sprites = pygame.sprite.Group()
 board = BoardEditor(w // 32, h // 32)
+Wall(50, 50, 0, all_sprites)
+Wall(50, 50, 180, all_sprites)
 running = True
 while running:
     for event in pygame.event.get():
@@ -79,7 +109,7 @@ while running:
             running = False
         elif event.type == pygame.MOUSEBUTTONUP:
             board.get_click(event.pos)
-
-    screen.fill((0, 0, 0))
+    screen.fill(BLACK)
     board.render(screen)
+    all_sprites.draw(screen)
     pygame.display.flip()
