431ae196bb440b46ea7922ec7022972e51db1540
Anastasia <nastya.nedoseicko@yandex.ru>
2019-12-28 18:25:10 +0300

    По идее класс рабочий, а так хз

--- Player.py
+++ Player.py
@@ -2,10 +2,6 @@ import pygame
 import os
 #import sys
 
-pygame.init()
-player_group = pygame.sprite.Group()
-player1 = None
-player2 = None
 def load_image(name):
     fullname = os.path.join('data', name)
     image = pygame.image.load(fullname).convert_alpha()
@@ -21,68 +17,50 @@ class Player(pygame.sprite.Sprite):
     image22 = load_image('totem22.png')
     image23 = load_image('totem23.png')
 
-
     def __init__(self, pos, flag=0):
         super().__init__([player_group, all_sprites])
         self.pos = pos
         self.flag = flag
-        if not flag:
-            self.image = Player.image1
-            self.rect = self.image.get_rect()
+        self.move = False
+        if not self.flag:
+            self.player_image = Player.image10
+            self.rect = self.player_image.get_rect()
         else:
-            self.image = Player.image2
-            self.rect = self.image.get_rect()
+            self.player_image  = Player.image20
+            self.rect = self.player_image.get_rect()
+
+    def set_move(self, move, direction=None):
+        if move:
+            if direction == RIGHT:
+                self.rect.x += 1
+            elif direction == LEFT:
+                self.rect.x -= 1
+            elif direction == UP:
+                self.rect.y -= 1
+            elif direction == DOWN:
+                self.rect.y += 1
+
+    def update(self, *args):
+        if not self.flag:
+            if self.player_image is self.image10:
+                self.player_image = self.image11
+            elif self.player_image is self.image11:
+                self.player_image = self.image12
+            elif self.player_image is self.image12:
+                self.player_image = self.image13
+            elif self.player_image is self.image13:
+                self.player_image = self.image10
+        else:
+            if self.player_image is self.image20:
+                self.player_image = self.image21
+            elif self.player_image is self.image21:
+                self.player_image = self.image22
+            elif self.player_image is self.image22:
+                self.player_image = self.image23
+            elif self.player_image is self.image23:
+                self.player_image = self.image20
 
 
 
-all_sprites = pygame.sprite.Group()
-tiles_group = pygame.sprite.Group()
-player_group = pygame.sprite.Group()
-tile_width = tile_height = 50
-
-#class Tile(pygame.sprite.Sprite):
-    #def __init__(self, tile_type, pos_x, pos_y):
-        #super().__init__(tiles_group, all_sprites)
-        #self.image = tile_images[tile_type]
-        #self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
-
-running = True
-clock = pygame.time.Clock()
-IsStart = False
-while running:
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN:
-            IsStart = True
-    if flag == 1:
-        if IsStart:
-            pressed = pygame.key.get_pressed()
-            if pressed[pygame.K_LEFT]:
-                player1.update((-tile_width, 0))
-            if pressed[pygame.K_RIGHT]:
-                player1.update((tile_width, 0))
-            if pressed[pygame.K_UP]:
-                player1.update((0, tile_width))
-            if pressed[pygame.K_DOWN]:
-                player1.update((0, tile_width))
-
-    else:
-        if IsStart:
-            pressed = pygame.key.get_pressed()
-            if pressed[pygame.K_LEFT]:
-                player2.update((-tile_width, 0))
-            if pressed[pygame.K_RIGHT]:
-                player2.update((tile_width, 0))
-            if pressed[pygame.K_UP]:
-                player2.update((0, tile_height))
-            if pressed[pygame.K_DOWN]:
-                player2.update((0, tile_height))
 
 
-    #screen.fill(pygame.Color(0, 0, 0))
-    #tiles_group.draw(screen)
-    #player_group.draw(screen)
-    #pygame.display.flip()
-    #clock.tick(FPS)
